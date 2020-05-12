bce1742a3484dc7da20f6155df6305435c5b7862
Stéphanie <stef-x06@hotmail.fr>
2020-05-12 21:48:25 +0200

    implement login inputs

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,10 +1,35 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './LoginPage.css';
 
 function LoginPage({isLoading, onLoginClick}) {
+  const [isEmailFocus, setIsEmailFocus] = useState(false);
+  const [emailValue, setEmailValue] = useState('');
+  const [passwordValue, setPasswordValue] = useState('');
+  const [isPasswordFocus, setIsPasswordFocus] = useState(false);
+
   function handleClick() {
     if (onLoginClick) {
       onLoginClick();
+    }
+  }
+
+  function onEmailFocus() {
+    setIsEmailFocus(true);
+  }
+
+  function onEmailBlur() {
+    if (!emailValue) {
+      setIsEmailFocus(false);
+    }
+  }
+
+  function onPasswordFocus() {
+    setIsPasswordFocus(true);
+  }
+
+  function onPasswordBlur() {
+    if (!passwordValue) {
+      setIsPasswordFocus(false);
     }
   }
 
@@ -14,12 +39,14 @@ function LoginPage({isLoading, onLoginClick}) {
         <section>
           <h1>Time to travel</h1>
           <form>
-            <label aria-label="email-field">
-              <input type="text" placeholder="Email"/>
-            </label>
-            <label aria-label="password-field">
-              <input type="text" placeholder="Password"/>
-            </label>
+            <div className="email-field">
+              <label className={isEmailFocus ? 'active' : ''} aria-label="email-field" onClick={onEmailFocus}>Email</label>
+              <input type="text" value={emailValue} onFocus={onEmailFocus} onBlur={onEmailBlur} onChange={e => setEmailValue(e.target.value)}/>
+            </div>
+            <div className="password-field">
+              <label className={isPasswordFocus ? 'active' : ''} aria-label="password-field" onClick={onPasswordFocus}>Password</label>
+              <input type="password" value={passwordValue} onFocus={onPasswordFocus} onBlur={onPasswordBlur} onChange={e => setPasswordValue(e.target.value)}/>
+            </div>
           </form>
           <button type="submit" onClick={handleClick}>Log in</button>
           <span>{isLoading ? 'isloading' : ''}</span>
