9590f2a60827b2fff4135bb4a5ece3605e68c870
Stéphanie <stef-x06@hotmail.fr>
2020-05-16 00:58:50 +0200

    redirect to home after login success

--- src/components/MainPage.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function MainPage() {
-  return <h1>Hello world!</h1>;
-}
-
-export default MainPage;
--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import {navigate} from 'hookrouter';
 import {useSelector, useDispatch} from 'react-redux';
 import LoginPage from '../components/LoginPage';
 import {fetchToken} from '../store/actions/login';
-import {getIsLoading, getStatusError} from '../store/selectors/login';
+import {getIsLoading, getStatusError, getToken} from '../store/selectors/login';
 
 function Login() {
   const dispatch = useDispatch();
   const isLoading = useSelector(getIsLoading);
   const statusError = useSelector(getStatusError);
+  const token = useSelector(getToken);
+
+  if (token) {
+    navigate('/home');
+  }
 
   function handleLoginClick(email, password) {
     dispatch(fetchToken(email, password));
