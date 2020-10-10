1006c92b09467bf2a3b4c3502abc3be26a63a628
Stéphanie <stef-x06@hotmail.fr>
2020-10-10 16:32:33 +0200

    handle registration frontend errors

--- src/components/LoginRegistration/RegistrationPage.js
+++ src/components/LoginRegistration/RegistrationPage.js
@@ -6,12 +6,16 @@ function RegistrationPage({registrationError, onRegistrationClick}) {
   const text = translations.registration;
   const [isEmailFocus, setIsEmailFocus] = useState(false);
   const [emailValue, setEmailValue] = useState('');
+  const [showEmailError, setShowEmailError] = useState(false);
   const [passwordValue, setPasswordValue] = useState('');
+  const [showPasswordError, setShowPasswordError] = useState(false);
   const [isPasswordFocus, setIsPasswordFocus] = useState(false);
   const [showPassword, setShowPassword] = useState(false);
+  const emailRegex = new RegExp('^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$');
+  const passwordRegex = new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,32})');
 
   function handleSubmitClick() {
-    if (onRegistrationClick) {
+    if (!showEmailError && !showPasswordError && onRegistrationClick) {
       onRegistrationClick(emailValue, passwordValue);
     }
   }
@@ -24,6 +28,8 @@ function RegistrationPage({registrationError, onRegistrationClick}) {
     if (!emailValue) {
       setIsEmailFocus(false);
     }
+
+    setShowEmailError(!emailRegex.test(emailValue));
   }
 
   function onPasswordFocus() {
@@ -34,6 +40,8 @@ function RegistrationPage({registrationError, onRegistrationClick}) {
     if (!passwordValue) {
       setIsPasswordFocus(false);
     }
+
+    setShowPasswordError(!passwordRegex.test(passwordValue));
   }
 
   function toggleShowPassword() {
@@ -53,16 +61,28 @@ function RegistrationPage({registrationError, onRegistrationClick}) {
           <img alt="world mappie logo" src="/public/assets/worldmappie.svg" width="120px"/>
           <h2>{text['h2_sign-up']}</h2>
           <form>
-            <div>
+            <div className="email-form">
               <label className={isEmailFocus ? 'active' : ''} aria-label="email-field" onClick={onEmailFocus}>{text.label_email}</label>
               <input type="text" value={emailValue} onFocus={onEmailFocus} onBlur={onEmailBlur} onChange={e => setEmailValue(e.target.value)}/>
             </div>
-            <div>
+            {showEmailError ? (
+              <error>
+                <div/>
+                <p>{showEmailError ? text.error_email : ''}</p>
+              </error>
+            ) : null}
+            <div className="password-form">
               <label className={isPasswordFocus ? 'active' : ''} aria-label="password-field" onClick={onPasswordFocus}>{text.label_password}</label>
               <input type={showPassword ? 'text' : 'password'} value={passwordValue} onFocus={onPasswordFocus} onBlur={onPasswordBlur} onChange={e => setPasswordValue(e.target.value)}/>
               <img hidden={showPassword} alt="eye show" src="/public/assets/password-show.svg" onClick={toggleShowPassword}/>
               <img hidden={!showPassword} alt="eye hide" src="/public/assets/password-hide.svg" onClick={toggleShowPassword}/>
             </div>
+            {showPasswordError ? (
+              <error>
+                <div/>
+                <p>{showPasswordError ? text.error_password : ''}</p>
+              </error>
+            ) : null}
           </form>
           {registrationError ? (
             <error>
